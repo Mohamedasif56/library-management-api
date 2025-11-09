94e0aea21ac91ecee8101f3686253bdecba70cbf
Mohamedasif56 <greatasif04@gmail.com>
2025-11-10 01:49:49 +0800

    Create BookRepository.cs
    repo: add repository interfaces + implementations

---
+++ Infrastructure/Repositories/BookRepository.cs
@@ -0,0 +1,46 @@
+using LibraryManagement.Application.Interfaces;
+using LibraryManagement.Domain.Entities;
+using LibraryManagement.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement.Infrastructure.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly LibraryDbContext _context;
+        public BookRepository(LibraryDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Book> AddAsync(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+            return book;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null) return false;
+
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<IEnumerable<Book>> GetAllAsync()
+            => await _context.Books.ToListAsync();
+
+        public async Task<Book?> GetByIdAsync(int id)
+            => await _context.Books.FindAsync(id);
+
+        public async Task<Book> UpdateAsync(Book book)
+        {
+            _context.Books.Update(book);
+            await _context.SaveChangesAsync();
+            return book;
+        }
+    }
+}
